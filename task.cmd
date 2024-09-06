95fd656ab119a6cd747f1a2a8c7a22e14ff2c363
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-09-06 09:52:26 +0900

    .

--- task.cmd
+++ task.cmd
@@ -2,14 +2,32 @@
 
 @echo off
 setlocal enabledelayedexpansion
+if not %UPDATE_ME% == "" (
+  curl --location --output - https://raw.githubusercontent.com/knaka/scr/main/task > %~f0
+  exit /b
+)
+set original_dir_path=%cd%
 set script_dir_path=%~dp0
 set script_name=%~n0
-set sh_dir_path=!script_dir_path!
 set env_file_path=!script_dir_path!\.env.sh.cmd
+set sh_dir_path=!script_dir_path!
 if exist !env_file_path! (
   call !env_file_path!
 )
-set toolbox_cmd_path=N/A
-call !sh_dir_path!\toolbox.lib.cmd
+set toolbox_cmd_path=undefined
+cd /d !sh_dir_path!
+set toolbox_file=toolbox.lib.cmd
+:begin
+  if exist !toolbox_file! (
+    call !toolbox_file!
+    goto :end
+  )
+  if %cd% == %cd:~0,3% (
+    exit /b 1
+  )
+  cd ..
+  goto :begin
+:end
+cd /d !original_dir_path!
 !toolbox_cmd_path! sh !sh_dir_path!\!script_name!.sh %*
 endlocal
