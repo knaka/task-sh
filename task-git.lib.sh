013f2d0dae0fe1d59a7f2666aee120664bb67b44
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-09-18 10:21:30 +0900

    .

--- task-git.lib.sh
+++ task-git.lib.sh
@@ -4,12 +4,12 @@ set_dir_sync_ignored "$(dirname "$0")"/.git
 
 subcmd_git() { # Run git command.
   cd "$(dirname "$0")" || exit 1
-  if ! test -d .git
+  if ! test -r .git/HEAD
   then
     git init
     git remote add origin git@github.com:knaka/src.git
-    git branch --set-upstream-to=origin/main main
-    git fetch origin
+    git fetch origin main
+    git reset --hard origin/main
   fi
   exec git "$@"
 }
