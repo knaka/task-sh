dc9d16040ef0fd32c44cd4f0cf889ece7a3cee79
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-09-30 13:52:39 +0900

    .

--- go/dm.go
+++ go/dm.go
@@ -15,7 +15,7 @@ func printable(ch byte) bool {
 	return 0x20 <= ch && ch < 0x7F
 }
 
-const chUnprintable = "."
+const chNotPrintable = "."
 
 const bytesPerLine = 16
 
@@ -26,50 +26,52 @@ const (
 
 const stdinFilename = "-"
 
+func dumpFile(filePath string, colored bool) {
+	var rawReader io.ReadCloser
+	if filePath == stdinFilename {
+		rawReader = os.Stdin
+	} else {
+		rawReader = V(os.Open(filePath))
+		defer (func() { V0(rawReader.Close()) })()
+	}
+	reader := bufio.NewReader(rawReader)
+	buf := make([]byte, bytesPerLine)
+	for addr := 0; ; addr += bytesPerLine {
+		pn := PR(reader.Read(buf)).NilIf(io.EOF)
+		if pn == nil {
+			break
+		}
+		var hexes []string
+		readable := ""
+		for i := 0; i < bytesPerLine; i++ {
+			if i < *pn {
+				hexes = append(hexes, fmt.Sprintf("%02X", buf[i]))
+				if printable(buf[i]) {
+					readable += string(buf[i])
+				} else {
+					if colored {
+						readable += escSeqRed + chNotPrintable + escSeqEnd
+					} else {
+						readable += chNotPrintable
+					}
+				}
+			} else {
+				hexes = append(hexes, "  ")
+				readable += " "
+			}
+		}
+		V0(fmt.Fprintf(os.Stdout, "%08X | %s | %s\n",
+			addr, strings.Join(hexes, " "), readable))
+	}
+}
+
 func main() {
 	Debugger()
 	if len(os.Args) == 1 {
 		os.Args = append(os.Args, stdinFilename)
 	}
+	colored := term.IsTerminal(int(os.Stdout.Fd()))
 	for _, arg := range os.Args[1:] {
-		func() {
-			var rawReader io.ReadCloser
-			if arg == stdinFilename {
-				rawReader = os.Stdin
-			} else {
-				rawReader = V(os.Open(arg))
-				defer (func() { V0(rawReader.Close()) })()
-			}
-			reader := bufio.NewReader(rawReader)
-			colored := term.IsTerminal(int(os.Stdout.Fd()))
-			buf := make([]byte, bytesPerLine)
-			for addr := 0; ; addr += bytesPerLine {
-				pn := PR(reader.Read(buf)).NilIf(io.EOF)
-				if pn == nil {
-					break
-				}
-				var hexes []string
-				readable := ""
-				for i := 0; i < bytesPerLine; i++ {
-					if i < *pn {
-						hexes = append(hexes, fmt.Sprintf("%02X", buf[i]))
-						if printable(buf[i]) {
-							readable += string(buf[i])
-						} else {
-							if colored {
-								readable += escSeqRed + chUnprintable + escSeqEnd
-							} else {
-								readable += chUnprintable
-							}
-						}
-					} else {
-						hexes = append(hexes, "  ")
-						readable += " "
-					}
-				}
-				V0(fmt.Fprintf(os.Stdout, "%08X | %s | %s\n",
-					addr, strings.Join(hexes, " "), readable))
-			}
-		}()
+		dumpFile(arg, colored)
 	}
 }
