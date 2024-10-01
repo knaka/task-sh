c56885c4265fc6c456b01f51dbb95366f8e5b4e7
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-10-01 21:20:52 +0900

    .

--- rs/src/subcmd_hello.rs
+++ rs/src/subcmd_hello.rs
@@ -2,28 +2,26 @@ use clap::{Command, Arg, ArgAction, arg};
 use crate::MainCommand;
 
 pub fn register(main_command: &mut MainCommand) {
-    main_command.register_subcommand(
-        Command::new("rs-hello")
-            .about("Greet the user")
-            .args([
-                (arg!(-n --name <NAME> "The name to greet") as Arg).default_value("world"),
-                Arg::new("name2")
-                    .short('N')
-                    .long("name2")
-                    .value_name("NAME")
-                    .help("The name to greet")
-                    .default_value("world")
-                    .required(false)
-                ,
-                Arg::new("uppercase")
-                    .short('u')
-                    .long("uppercase")
-                    .action(ArgAction::SetTrue)
-                    .help("Uppercase the output")
-                ,
-            ])
-        ,
-        |matches: &clap::ArgMatches| {
+    let command = Command::new("rs-hello")
+        .about("Greet the user")
+        .args([
+            (arg!(-n --name <NAME> "The name to greet") as Arg).default_value("world"),
+            Arg::new("name2")
+                .short('N')
+                .long("name2")
+                .value_name("NAME")
+                .help("The name to greet")
+                .default_value("world")
+                .required(false),
+            Arg::new("uppercase")
+                .short('u')
+                .long("uppercase")
+                .action(ArgAction::SetTrue)
+                .help("Uppercase the output")
+                .required(false),
+        ]);
+    main_command.register_subcommand(command,
+    |matches: &clap::ArgMatches| {
             let uppercase = matches.get_flag("uppercase");
             let name = matches.get_one::<String>("name").unwrap();
             if uppercase {
@@ -33,5 +31,5 @@ pub fn register(main_command: &mut MainCommand) {
             }
             Ok(())
         },
-    );
+    )
 }
