ecb3e14e60fd7e6ca0d553058ee42a36ddc3ba8f
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-10-01 19:01:31 +0900

    .

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,11 +1,9 @@
 use clap::{Command};
-use once_cell::sync::Lazy;
 use std::collections::HashMap;
-use std::sync::Mutex;
 
 type CommandHandler = fn(&clap::ArgMatches) -> Result<(), Box<dyn std::error::Error>>;
 
-struct MainCommand {
+pub struct MainCommand {
     root: Command,
     map: HashMap<&'static str, CommandHandler>,
 }
@@ -18,29 +16,25 @@ impl MainCommand {
         }
     }
 
-    fn register(&mut self, name: &'static str, subcommand: Command, handler: CommandHandler) {
+    fn register_subcommand(&mut self, name: &'static str, subcommand: Command, handler: CommandHandler) {
         self.root = self.root.clone().subcommand(subcommand);
         self.map.insert(name, handler);
     }
 }
 
-static ROOT_COMMAND: Lazy<Mutex<MainCommand>> = Lazy::new(|| {
-    let root_command = Command::new("myapp")
-        .about("My super CLI app")
-        .version("0.1.0");
-    Mutex::new(MainCommand::new(root_command))
-});
-
 include!("subcmds.rs");
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let main_command = ROOT_COMMAND.lock().unwrap();
+    let mut main_command = MainCommand::new(
+        Command::new(""),
+    );
+    register_subcommands(&mut main_command);
     let mut root_command = main_command.root.clone();
-    let matches = root_command.clone().get_matches();
-    if matches.subcommand().is_none() {
+    if std::env::args().count() == 1 {
         root_command.print_help()?;
         std::process::exit(0);
     }
+    let matches = root_command.get_matches();
     if let Some((subcommand_name, subcommand_matches)) = matches.subcommand() {
         let handler = main_command.map.get(subcommand_name);
         if let Some(handler) = handler {
--- rs/src/subcmd_hello.rs
+++ rs/src/subcmd_hello.rs
@@ -1,19 +1,12 @@
-use ctor::ctor;
-use crate::ROOT_COMMAND;
 use clap::{Command, Arg, ArgAction, arg};
+use crate::MainCommand;
 
-#[ctor]
-fn init() {
+pub fn register(main_command: &mut MainCommand) {
     const NAME: &str = "rs-hello";
-    // clap::_tutorial - Rust https://docs.rs/clap/4.5.18/clap/_tutorial/index.html
-    // let args: Vec<Arg> = vec![];
-    // let arg: Arg = arg!(-v --verbose "Print test information verbosely"); arg.default_value("xxx").required(false);
-    // args.push(arg);
-    ROOT_COMMAND.lock().unwrap().register(
+    main_command.register_subcommand(
         NAME,
         Command::new(NAME)
-            .about("Greets the user.")
-            // .arg(arg!(-n --name <NAME> "The name to greet").default_value("world").required(false))
+            .about("Greet the user")
             .args([
                 (arg!(-n --name <NAME> "The name to greet") as Arg).default_value("world"),
                 Arg::new("name2")
