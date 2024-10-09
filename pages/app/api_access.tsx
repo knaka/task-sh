568a10bf94bed9eb4633379d462768547fc5b3fe
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-10-09 22:55:40 +0900

    .

--- pages/app/api_access.tsx
+++ pages/app/api_access.tsx
@@ -3,7 +3,12 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-const apiEndpointBase = process.env.NEXT_PUBLIC_API_PORT && `http://127.0.0.1:${process.env.NEXT_PUBLIC_API_PORT}/api` || "/api";
+import { hc } from "hono/client";
+import { AppType2 } from "../worker/index";
+
+const apiEndpointBase = process.env.NEXT_PUBLIC_API_PORT && `http://127.0.0.1:${process.env.NEXT_PUBLIC_API_PORT}/` || "/";
+
+const client = hc<AppType2>(apiEndpointBase);
 
 console.log("apiEndpointBase", apiEndpointBase);
 
@@ -12,9 +17,15 @@ export const ApiAccess = () => {
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(`${apiEndpointBase}/hello`);
-      const data: any = await response.json();
-      setMessage(data.message);
+      // const response = await fetch(`${apiEndpointBase}/hello`);
+      // const data: any = await response.json();
+      // setMessage(data.message);
+
+      // const response2 = await client.hello.$get();
+      const response2 = await client.api.hello.$get({ param: {} });
+      setMessage((await response2.json()).message);
+
+      // const response3 = await client.api.world.$get();
     })();
   });
   return (<div>
--- pages/worker/index.ts
+++ pages/worker/index.ts
@@ -2,6 +2,11 @@ import {
   Hono,
 } from "hono";
 
+import { cors } from "hono/cors";
+
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+
 // import {
 //   EventContext,
 // } from "hono/cloudflare-pages"
@@ -12,23 +17,44 @@ type Bindings = {
   },
 };
 
-const app = new Hono<{ Bindings: Bindings }>();
+// app.use("*", (c, next) => {
+//   c.res.headers.append("Access-Control-Allow-Origin", "*");
+//   c.res.headers.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   return next();
+// })
 
-app.use("*", (c, next) => {
-  c.res.headers.append("Access-Control-Allow-Origin", "*");
-  c.res.headers.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  return next();
-})
+// CORS Middleware - Hono https://hono.dev/docs/middleware/builtin/cors
+const api = new Hono()
+  .get("/hello",
+    zValidator(
+      "param",
+      z.object({}),
+    ),
+    (c) => {
+      return c.json({
+        message: "Hello Pages!! This is Hono!! 321fab3!",
+      })
+    }
+  )
+  .get("/world",
+    (c) => {
+      return c.json({
+        message: "World!!",
+      })
+    },
+  )
+;
 
-app.get("/api/hello", (c) => {
-  return c.json({
-    message: "Hello Pages!! This is Hono!! 7b57ee2!",
-  });
-});
+const root = new Hono<{ Bindings: Bindings }>()
+  .use("*", cors())
+  .route("/api", api)
+  .get("*", async (c) => {
+    const res = await c.env.ASSETS.fetch(c.req.raw);
+    return res;
+  })
+;
 
-app.get("*", async (c) => {
-  const res = await c.env.ASSETS.fetch(c.req.raw);
-  return res;
-});
+export default root;
+// export type AppType = typeof routes;
+export type AppType2 = typeof root;
 
-export default app;
