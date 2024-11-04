25d4c726788d6d9f41046de706dd21f4e7b3b809
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-04 11:20:17 +0900

    .

--- sh/sh-test-errexit.sh
+++ sh/sh-test-errexit.sh
@@ -10,18 +10,18 @@ test "${guard_429c89f+set}" = set && return 0; guard_429c89f=x
 assert_match "^errexit[[:space:]]+on$" "$(set -o | grep -E '^errexit\b')"
 
 # Fails to restore the state if it was not saved.
-assert_false restore_state
+assert_false restore_shell_flags
 
 # Save the state.
-backup_state
-assert_match '^set -o errexit$' "$original_state_c225b8f"
+backup_shell_flags
+assert_match '^set -o errexit$' "$shell_flags_c225b8f"
 
 # Fails to save the state if it was already saved. Does not nest.
-assert_false backup_state
+assert_false backup_shell_flags
 
 # Turn it off.
 set +o errexit
 assert_match "^errexit[[:space:]]+off$" "$(set -o | grep -E '^errexit\b')"
 
 # Restore the state.
-restore_state
+restore_shell_flags
