77fc0fe751885c302f524f0f71890dbf707bcedf
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-07 19:56:18 +0900

    .

--- go/task-go.lib.sh
+++ go/task-go.lib.sh
@@ -7,31 +7,53 @@ test "${guard_1e6bc22+set}" = set && return 0; guard_1e6bc22=-
 # Returns the path to the Go root directory.
 goroot_path() (
   # All releases - The Go Programming Language https://go.dev/dl/
-  required_min_ver=go1.23.1
+  required_min_ver=go1.23.0
 
-  if test "${GOROOT+set}" && type "$GOROOT"/bin/go > /dev/null 2>&1 && version_ge "$("$GOROOT"/bin/go env GOVERSION)" "$required_min_ver"
+  # Search for Go SDK which fulfills the minimum version requirement.
+  psv_go_dir_paths=
+  # $GOROOT
+  if test "${GOROOT+set}" = set
   then
-    echo "$GOROOT"
-    return
+    psv_go_dir_paths="$(array_append "$psv_go_dir_paths" "|" "$(realpath "$GOROOT")")"
   fi
-  if which go > /dev/null 2>&1 && version_ge "$(go env GOVERSION)" "$required_min_ver"
+  # `go` command
+  if type go > /dev/null 2>&1
   then
-    go env GOROOT
-    return
+    psv_go_dir_paths="$(array_append "$psv_go_dir_paths" "|" "$(realpath "$(go env GOROOT)")")"
   fi
-  for dir_path in \
-    "$HOME"/sdk/${required_min_ver} \
-    /usr/local/go \
-    /usr/local/Cellar/go/* \
-    "C:/Program Files"/Go \
-    "$HOME"/go
+  # System-wide installation
+  if is_windows
+  then
+    psv_go_dir_paths="$(array_append "$psv_go_dir_paths" "|" "C:/Program Files/Go")"
+  else
+    psv_go_dir_paths="$(array_append "$psv_go_dir_paths" "|" "/usr/local/go")"
+  fi
+  # Automatically installed SDKs
+  psv_sdk_go_dir_paths=
+  for dir_path in "$HOME"/sdk/go*
   do
-    if type "$dir_path"/bin/go > /dev/null 2>&1 && version_ge "$("$dir_path"/bin/go env GOVERSION)" "$required_min_ver"
+    if ! test -d "$dir_path"
     then
-      echo "$dir_path"
+      continue
+    fi
+    psv_sdk_go_dir_paths="$(array_append "$psv_sdk_go_dir_paths" "|" "$dir_path")"
+  done
+  # Sort the SDKs in descending order.
+  psv_sdk_go_dir_paths="$(array_sort "$psv_sdk_go_dir_paths" "|" sort_version -r)"
+  psv_go_dir_paths="$(array_append "$psv_go_dir_paths" "|" "$psv_sdk_go_dir_paths")"
+  # Find the first Go SDK which fulfills the minimum version requirement.
+  set_ifs_pipe
+  for go_dir_path in $psv_go_dir_paths
+  do
+    if type "$go_dir_path"/bin/go > /dev/null 2>&1 && version_ge "$("$go_dir_path"/bin/go env GOVERSION)" "$required_min_ver"
+    then
+      echo "$go_dir_path"
       return
     fi
   done
+  restore_ifs
+
+  # If Go SDK is not found, download and install it.
   sdk_dir_path="$HOME"/sdk
   goroot="$sdk_dir_path"/${required_min_ver}
   case "$(uname -s)" in
@@ -68,10 +90,8 @@ goroot_path() (
 # Sets the Go environment. If CGO is required, call `set_unixy_dev_env` also.
 set_go_env() {
   first_call 1dc30dd || return 0
-  # GOROOT="$(goroot_path)"
-  # export GOROOT
   unset GOROOT
-  echo Using Go compiler in "$(goroot_path)"/bin >&2
+  echo Using Go compiler in "$(goroot_path)" >&2
   PATH="$(array_prepend "$PATH" : "$(goroot_path)"/bin)"
   export PATH
 }
