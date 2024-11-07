7b4022638c63b9c8eaa6d1a4664425da97bbbf1d
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-07 14:19:52 +0900

    .

--- go/go-embedded.cmd
+++ go/go-embedded.cmd
@@ -8,86 +8,137 @@ if "%~1" == "update-me" (
 )
 
 @REM All releases - The Go Programming Language https://go.dev/dl/
-set "ver=1.23.1"
+set required_min_major_ver=1
+set required_min_minor_ver=23
+set ver=!required_min_major_ver!.!required_min_minor_ver!.1
 
-set "exit_code=1"
+set exit_code=1
 
 :unique_temp_loop
 set "temp_dir_path=%TEMP%\%~n0-%RANDOM%"
 if exist "!temp_dir_path!" goto unique_temp_loop
 mkdir "!temp_dir_path!" || goto :exit
+call :to_short_path "!temp_dir_path!"
+set temp_dir_spath=!short_path!
+
+set goroot_dir_spath=
+
+call :to_short_path "C:\Program Files"
+set program_files_spath=!short_path!
+call :to_short_path "%USERPROFILE%"
+set user_profile_spath=!short_path!
 
 @REM Command in %PATH%
-where go >nul 2>&1
+where go >nul 2>&1 
 if !ERRORLEVEL! == 0 (
-    set "go_cmd_path=go"
-    goto found_go_cmd
-)
-@REM Trivial installation paths
-set "dirs=%USERPROFILE%\go\go!ver!\bin;%USERPROFILE%\sdk\go!ver!\bin;\Program Files\Go\bin"
-for %%d in ("!dirs:;=" "!") do (
-    if exist "%%d\go.exe" (
-        set "go_cmd_path=%%d\go.exe"
-        goto found_go_cmd
+  for /F "usebackq tokens=*" %%p in (`where go`) do (
+    call :to_short_path "%%p"
+    set cmd_spath=!short_path!
+    call :set_proper_goroot_dir_spath !cmd_spath!
+    if !goroot_dir_spath! neq "" (
+      goto :found_goroot
     )
+  )
 )
-@REM Download if not found
-set "goos=windows"
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    set "goarch=amd64"
-) else if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
-    set "goarch=amd64"
-) else (
-    goto :exit
-)
-set "sdk_dir_path=%USERPROFILE%\sdk"
-if not exist "!sdk_dir_path!" (
-    mkdir "!sdk_dir_path!" || goto :exit
-)
-set "zip_path=!temp_dir_path!\go.zip"
-curl.exe --fail --location -o "!zip_path!" "https://go.dev/dl/go!ver!.%goos%-%goarch%.zip" || goto :exit
-cd "!sdk_dir_path!" || goto :exit
-tar.exe -xf "!zip_path!" || goto :exit
-move /y "!sdk_dir_path!\go" "!sdk_dir_path!\go!ver!" || goto :exit
-set "go_cmd_path=!sdk_dir_path!\go!ver!\bin\go.exe"
 
-:found_go_cmd
-if not defined GOPATH (
-    set "GOPATH=%USERPROFILE%\go"
+@REM Trivial installation paths
+@REM for /D %%d in (!program_files_spath!\go !user_profile_spath!\sdk\go*) do (
+for /D %%d in (!user_profile_spath!\sdk\go*) do (
+  set cmd_spath=%%d\bin\go.exe
+  if exist !cmd_spath! (
+    call :set_proper_goroot_dir_spath !cmd_spath!
+    if !goroot_dir_spath! neq "" (
+      goto :found_goroot
+    )
+  )
 )
-if not exist "!GOPATH!\bin" (
-    mkdir !%GOPATH!\bin"
+
+@REM Download if not found
+set goos=windows
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+  set goarch=amd64
+) else if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
+  set goarch=amd64
+) else (
+  goto :exit
+)
+set sdk_dir_spath=!user_profile_spath!\sdk
+if not exist !sdk_dir_spath! (
+  mkdir !sdk_dir_spath! || goto :exit
+)
+set zip_spath=!temp_dir_spath!\go.zip
+echo Downloading Go SDK: !ver! >&2
+curl.exe --fail --location -o !zip_spath! "https://go.dev/dl/go!ver!.%goos%-%goarch%.zip" || goto :exit
+cd !sdk_dir_spath! || goto :exit
+unzip -q !zip_spath! || goto :exit
+move /y !sdk_dir_spath!\go !sdk_dir_spath!\go!ver! || goto :exit
+set goroot_dir_spath=!sdk_dir_spath!\go!ver!
+
+:found_goroot
+
+set go_cmd_spath=!goroot_dir_spath!\bin\go.exe
+set GOROOT=
+
+if not defined GOPATH (
+  set GOPATH=!user_profile_spath!\go
+)
+
+if not exist !GOPATH!\bin (
+  mkdir !GOPATH!\bin
 )
 
 set "name=embedded-%~f0"
+set "name=!name: =_!"
 set "name=!name:\=_!"
 set "name=!name::=_!"
 set "name=!name:/=_!"
-if exist "!GOPATH!\bin\!name!.exe" (
-    xcopy /l /d /y "!GOPATH!\bin\!name!.exe" "%~f0" | findstr /b /c:"1 " >nul 2>&1
-    if !ERRORLEVEL! == 0 (
-        goto :execute
-    )
+if exist !GOPATH!\bin\!name!.exe (
+  xcopy /l /d /y !GOPATH!\bin\!name!.exe "%~f0" | findstr /b /c:"1 " >nul 2>&1
+  if !ERRORLEVEL! == 0 (
+    goto :execute
+  )
 )
 
 :build
+echo Using Go compiler: !go_cmd_spath! >&2
 for /f "usebackq tokens=1 delims=:" %%i in (`findstr /n /b :embed_53c8fd5 "%~f0"`) do set n=%%i
-set "tempfile=!temp_dir_path!\!name!.go"
-more +%n% "%~f0" > "!tempfile!"
+set tempfile=!temp_dir_spath!\!name!.go
+more +%n% "%~f0" > !tempfile!
 
-!go_cmd_path! build -o !GOPATH!\bin\!name!.exe "!tempfile!" || goto :exit
-del /q "!temp_dir_path!"
-goto :execute
+!go_cmd_spath! build -o !GOPATH!\bin\!name!.exe !tempfile! || goto :exit
+del /q !temp_dir_spath!
 
 :execute
 !GOPATH!\bin\!name!.exe %* || goto :exit
-set "exit_code=0"
+set exit_code=0
 
 :exit
-if exist "!temp_dir_path!" (
-    del /q "!temp_dir_path!"
+if exist !temp_dir_spath! (
+  del /q !temp_dir_spath!
 )
 exit /b !exit_code!
+
+:to_short_path
+set "input_path=%~1"
+for %%i in ("%input_path%") do set "short_path=%%~si"
+exit /b
+goto :eof
+
+:set_proper_goroot_dir_spath
+for /F "usebackq tokens=*" %%v in (`%1 env GOVERSION`) do (
+  set version=%%v
+  set major=!version:~2,1!
+  set minor=!version:~4,2!
+  if !major! geq !required_min_major_ver! (
+  if !minor! geq !required_min_minor_ver! (
+    for /F "useback tokens=*" %%p in (`%1 env GOROOT`) do (
+      call :to_short_path "%%p"
+      set goroot_dir_spath=!short_path!
+    )
+  )
+)
+exit /b
+goto :eof
 
 endlocal
 
@@ -99,10 +150,10 @@ import (
 )
 
 func main() {
-    println("Hello, World!2")
+    println("Hello, World!")
     for i, arg := range os.Args {
         println(i, arg)
     }
     cwd, _ := os.Getwd()
-    println(cwd)
+    println("CWD:", cwd)
 }
