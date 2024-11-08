c5092e73b28d9802bf81fc0c3ad08948b951a9ca
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-08 22:13:07 +0900

    .

--- task-prj.lib.sh
+++ task-prj.lib.sh
@@ -23,7 +23,25 @@ prompt() {
   done
 }
 
+cli_usage() {
+  echo "exit: Exit the program."
+}
+
 task_cli() {
-  read -e s
-  echo d: $s  
+  while true
+  do
+    printf "> "
+    IFS= read -r line
+    case "$line" in
+      (exit) break ;;
+      ("") cli_usage ;;
+      (*)
+        backup_shell_flags
+        set +o errexit
+        eval "$line"
+        echo "exit status: $?" >&2
+        restore_shell_flags
+        ;;
+    esac
+  done
 }
