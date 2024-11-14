7ee7535b46c9e9b0e6016d799e96dfd1329228d3
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-14 18:06:11 +0900

    .

--- pages/task-prj.lib.sh
+++ pages/task-prj.lib.sh
@@ -25,11 +25,11 @@ subcmd_esbuild() {
 }
 
 # shellcheck disable=SC2120
-task_worker__build() {
+task_worker__build() { # Build the worker files into a JS file.
   subcmd_esbuild "$@" --bundle worker/index.ts --format=esm --outfile=public/_worker.js
 }
 
-task_worker__depbuild() {
+task_worker__depbuild() { # Build the worker files if the source files are newer than the output files.
   if ! newer worker/ --than public/_worker.js
   then
     return 0
@@ -37,7 +37,7 @@ task_worker__depbuild() {
   task_worker__build
 }
 
-task_worker__watchbuild() {
+task_worker__watchbuild() { # Watch the worker files and build them into JS files.
   # "forever" to keep the process running even after the stdin is closed.
   subcmd_esbuild "$@" --bundle worker/index.ts --format=esm --outfile=public/_worker.js --watch=forever
 }
@@ -47,7 +47,7 @@ subcmd_wrangler() {
   node"$(exe_ext)" node_modules/wrangler/bin/wrangler.js "$@"
 }
 
-task_worker__dev() {
+task_worker__dev() { # Launch the Worker service in the development mode.
   load_env
   opt_9754aa0=
   if test "${NEXT_PUBLIC_API_PORT+set}" = set
@@ -58,12 +58,12 @@ task_worker__dev() {
   subcmd_wrangler pages dev $opt_9754aa0 --live-reload --show-interactive-dev-session=false public/
 }
 
-task_next__build() {
+task_next__build() { # Build the Next.js project.
   subcmd_next build "$@"
 }
 
 # shellcheck disable=SC2120
-task_next__depbuild() {
+task_next__depbuild() { # Build the Next.js project if the source files are newer than the output files.
   if ! newer app/ public/ --than build/out
   then
     return 0
@@ -91,7 +91,7 @@ next_prompt() {
   done
 }
 
-task_next__dev() {
+task_next__dev() { # Launch the Next.js development server.
   load_env
   opts_93039d0=
   if test "${NEXT_DEV_SERVER_PORT+set}" = set
@@ -103,7 +103,7 @@ task_next__dev() {
   subcmd_next dev $opts_93039d0
 }
 
-task_dev() {
+task_dev() { # Launch the development servers.
   NODE_ENV=development
   export NODE_ENV
   APP_ENV=development
@@ -123,11 +123,11 @@ task_dev() {
   next_prompt "http://localhost:${NEXT_DEV_SERVER_PORT:-3000}"
 }
 
-task_pages__start() {
+task_pages__start() { # Launch the Wrangler Pages development server.
   subcmd_wrangler pages dev build/out/
 }
 
-task_start() {
+task_start() { # Launch the Pages preview server.
   NODE_ENV=production
   export NODE_ENV
   APP_ENV=production
@@ -162,15 +162,15 @@ task_dev__db__dump() { # Dump the development database.
   subcmd_wrangler d1 export --local --output=/dev/stdout test-db
 }
 
-task_dev__db__drop() {
+task_dev__db__drop() { # Drop the development database.
   find .wrangler -type f -name "*.sqlite*" -print0 | xargs -0 -n1 rm -f
 }
 
-task_dev__db__create() {
+task_dev__db__create() { # Create the development database.
   subcmd_dev__db__exec --command "SELECT current_timestamp"
 }
 
-task_dev__db__diff() {
+task_dev__db__diff() { # Generate the schema difference between the development database and the schema file.
   task_dev__db__schema
   rm -f build/dev-schema.db
   subcmd_sqlite3 build/dev-schema.db < build/dev-schema.sql
@@ -178,17 +178,22 @@ task_dev__db__diff() {
   cat build/dev-diff.sql
 }
 
-task_dev__db__migrate() {
+task_dev__db__migrate() { # Apply the schema changes to the development database.
   task_dev__db__diff
+  if test "$(sha1sum build/dev-diff.sql | field 1)" = e7efbf38cff7d12493cbe795586c588dccb332f4
+  then
+    echo "No schema changes."
+    return 0
+  fi
   subcmd_dev__db__exec --file=build/dev-diff.sql
 }
 
-task_db__gen() {
+task_db__gen() { # Generate the database access layer (./sqlcgen/*).
   cross_run ./cmd-gobin run sqlc generate
   # To keep the trailing spaces.
   ifs_null
   # shellcheck disable=SC2043
-  for file in sqlcgen/querier.ts
+  for file_path in sqlcgen/querier.ts
   do
     while read -r line
     do
@@ -205,13 +210,13 @@ task_db__gen() {
         line="$(echo "$line" | sed -E -e "s/\.bind\(.*\)/.bind($params)/")"
       fi
       echo "$line"
-    done < "$file" > "$file.tmp"
-    mv "$file.tmp" "$file"
+    done < "$file_path" > "$file_path.tmp"
+    mv "$file_path.tmp" "$file_path"
   done
   ifs_restore
 }
 
-task_db__watchgen() {
+task_db__watchgen() { # Watch the SQL files and generate the database access layer (./sqlcgen/*).
   while true
   do
     if newer query.sql schema.sql --than sqlcgen/
