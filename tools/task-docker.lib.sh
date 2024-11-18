8bb1818a357dcc985c95afdf0b964ee58bc1ef69
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-18 10:26:11 +0900

    .

---
+++ tools/task-docker.lib.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -o nounset -o errexit
+
+test "${guard_3b8c583+set}" = set && return 0; guard_3b8c583=-
+
+. ./task.sh
+
+subcmd_docker() (
+  if ! type docker > /dev/null 2>&1
+  then
+    echo "Docker is not installed. Exiting." >&2
+    exit 1
+  fi
+  if ! docker version > /dev/null 2>&1
+  then
+    echo "Docker is not availabe. Exiting." >&2
+    exit 1
+  fi
+  docker "$@"
+)
+
+task_docker__status() {
+  subcmd_docker info
+}
+
+task_docker__start() {
+  if task_docker__status >/dev/null 2>&1
+  then
+    echo "Docker is already running." >&2
+    return 0
+  fi
+  echo "Docker is not running. Starting Docker." >&2
+  if is_darwin
+  then
+    open --background -a Docker
+    while true
+    do
+      sleep 1
+      if task_docker__status >/dev/null 2>&1
+      then
+        echo "Docker has started." >&2
+        break
+      fi
+    done
+  else
+    echo "Not implemented."
+    return 1
+  fi
+}
