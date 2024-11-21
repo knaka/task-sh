5f3aa4ec9a756b0d6c0b8e01934ef7d836528035
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-11-21 17:35:31 +0900

    .

--- pages/worker/users/[[all]].tsx
+++ pages/worker/users/[[all]].tsx
@@ -2,29 +2,61 @@ import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 import { renderToString } from 'react-dom/server'
 import RootLayout from '../../app/layout'
+import { useState, useEffect } from 'react'
+import { getUser } from "../../sqlcgen/querier"
+import { D1Database } from "@cloudflare/workers-types";
 
-const app = new Hono()
+// const app = new Hono()
+export const app = new Hono<{ Bindings: Bindings }>();
 
 interface SiteData {
   title: string
   children?: any
 }
 
-const Content = (props: { siteData: SiteData; name: string }) => {
+const Content = (props: { siteData: SiteData; name: string, userName: string }) => {
+  // const [userName, setUserName] = useState("")
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await getUser(props.db, { nullableId: 3 })
+  //     if (res) {
+  //       console.log("User found")
+  //       const msg = `User: ${res.id} ${res.username}`
+  //       setUserName(msg)
+  //     } else {
+  //       console.log("User not found")
+  //     }
+  //   })();
+  // }, []);
   return <RootLayout {...props.siteData}>
     <>
-      <h1>Hello 3fac81d {props.name} {}</h1>
+      <h1>Hello 3fac81d {props.name} and {props.userName}</h1>
     </>
   </RootLayout>
 };
 
-app.get('/:name', (c) => {
+type Bindings = {
+  ASSETS: {
+    fetch: typeof fetch;
+  },
+  DB: D1Database,
+};
+
+app.get('/:name', async (c) => {
   const { name } = c.req.param()
+
+  let userName = ""
+  const res = await getUser(c.env.DB, { nullableId: 3 })
+  if (res) {
+    userName = `User: ${res.id} ${res.username}`
+  }
+
   const props = {
     name: name,
     siteData: {
       title: 'JSX with html sample',
     },
+    userName,
   }
   return c.html(renderToString(<Content {...props} />))
 })
