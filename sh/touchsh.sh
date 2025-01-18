007ab72a22462ba6e0c2937ce0796a921104aa29
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-18 22:38:21 +0900

    .

--- sh/touchsh.sh
+++ sh/touchsh.sh
@@ -31,7 +31,6 @@ fi
 unique_id="$(sh "$(dirname "$0")"/rand7.sh)"
 cat <<EOF > "$1"
 #!/bin/sh
-# shellcheck disable=SC3043
 test "\${guard_${unique_id}+set}" = set && return 0; guard_${unique_id}=x
 set -o nounset -o errexit
 EOF
