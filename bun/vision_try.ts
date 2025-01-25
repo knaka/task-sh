bfdd4efba689ab0af7cd90048974b79923097602
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-25 14:07:15 +0900

    .

--- bun/vision_try.ts
+++ bun/vision_try.ts
@@ -1,5 +1,7 @@
-import { createStreamWithTemplate } from './template_stream';
-import { base64Transform } from './base64_stream';
+// bun vision_try.ts AIz... test.jpg
+
+import { createStreamWithTemplate } from '@lib/template_stream';
+import { base64Transform } from '@lib/base64_stream';
 
 const apiKey = Bun.argv[2];
 const imageFilePath = Bun.argv[3];
@@ -7,37 +9,31 @@ const imageFilePath = Bun.argv[3];
 const imageStream = Bun.file(imageFilePath).stream();
 const visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
 
-const requestBodyTemplate = JSON.stringify({
-  "requests": [
-    {
-      "image": { "content": "aa7c245" },
-      "features": [
-        {
-          "type": "TEXT_DETECTION",
-          "maxResults": 10,
-        },
-      ],
-    },
-  ],
-});
-
-const base64ImageStream = imageStream.pipeThrough(base64Transform);
-
-const requestBodyStream = createStreamWithTemplate(requestBodyTemplate, "aa7c245", base64ImageStream);
-
-// console.log(await (new Response(bodyStream).text()));
-
 const resp = await fetch(visionApiUrl, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-  body: requestBodyStream,
+  body: createStreamWithTemplate(JSON.stringify({
+    "requests": [
+      {
+        "image": { "content": "aa7c245" },
+        "features": [
+          {
+            "type": "TEXT_DETECTION",
+            "maxResults": 10,
+          },
+        ],
+      },
+    ],
+  }), "aa7c245", imageStream.pipeThrough(base64Transform)),
 });
+
+// console.log(await (new Response(bodyStream).text()));
 
 if (! resp.ok) {
   throw new Error(`Failed to fetch: ${resp.status} ${resp.statusText} ${await resp.text()}`);
 }
 
-const jsonResp = await resp.json();
-console.log('Response:', jsonResp);
+const respBody = await resp.json();
+console.log('Response:', respBody);
