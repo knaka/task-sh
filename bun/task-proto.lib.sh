2d8b9ec7b6c8c74d9ae9ee48ed16b47b24ea515b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-25 09:08:58 +0900

    .

---
+++ bun/task-proto.lib.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+test "${guard_a710579+set}" = set && return 0; guard_a710579=x
+set -o nounset -o errexit
+
+# To be implemented
+# proto - A multi-language version manager | moonrepo https://moonrepo.dev/proto
+# Releases · moonrepo/proto https://github.com/moonrepo/proto/releases
