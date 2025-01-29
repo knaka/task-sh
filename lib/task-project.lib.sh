17c389df6edebc5e325f71489f1d878f35f2dec6
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-29 23:41:07 +0900

    .

--- lib/task-project.lib.sh
+++ lib/task-project.lib.sh
@@ -2,18 +2,19 @@
 test "${guard_fb8b13a+set}" = set && return 0; guard_fb8b13a=-
 
 . ./task.sh
-. ./task-bun.lib.sh
-. ./task-sqlc.lib.sh
-  set_sqlc_version "v1.27.0"
-. ./task-sqlc-ts.lib.sh
 . ./task-pages.lib.sh
   # Only “Catch All” routes are the transpile targets.
   set_pages_functions_src_pattern "./src-pages/functions/**/[*.ts"
-. ./task-astro.lib.sh
+
+. ./task-bun.lib.sh
 
 subcmd_test() { # Run tests.
   subcmd_bun test "$@"
 }
+
+. ./task-sqlc.lib.sh
+  set_sqlc_version "v1.27.0"
+. ./task-sqlc-ts.lib.sh
 
 task_db__gen() { # Generate the database access layer (./db/sqlcgen/*).
   subcmd_sqlc generate --file ./db/sqlc.yaml  
