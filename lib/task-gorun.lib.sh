29698069b7b44abf6f1fcbd816624dbfaf0b0210
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-29 12:26:25 +0900

    .

--- lib/task-gorun.lib.sh
+++ lib/task-gorun.lib.sh
@@ -4,10 +4,10 @@ test "${guard_932ee57+set}" = set && return 0; guard_932ee57=-
 . ./task.sh
 
 # Tags · knaka/gorun https://github.com/knaka/gorun/tags
-: "${gorun_version:=v0.1.1}"
+: "${version_37475d8:=v0.1.1}"
 
 set_gorun_version() {
-  gorun_version="$1"
+  version_37475d8="$1"
 }
 
 subcmd_gorun() { # Executes Go main package `pkg_name@ver` with arguments and cache the binary to reuse.
@@ -19,11 +19,11 @@ subcmd_gorun() { # Executes Go main package `pkg_name@ver` with arguments and ca
   then
     cmd_ext=.cmd
   fi
-  local cmd_base=gorun@"$gorun_version""$cmd_ext"
+  local cmd_base=gorun@"$version_37475d8""$cmd_ext"
   local cmd_path="$app_dir_path"/"$cmd_base"
   if ! test -x "$cmd_path"
   then
-    cross_run curl --fail --location --output "$cmd_path" https://raw.githubusercontent.com/knaka/gorun/refs/tags/"$gorun_version"/gorun"$cmd_ext"
+    cross_run curl --fail --location --output "$cmd_path" "https://raw.githubusercontent.com/knaka/gorun/refs/tags/${version_37475d8}/gorun${cmd_ext}"
     chmod +x "$cmd_path"
   fi
   "$cmd_path" "$@"
