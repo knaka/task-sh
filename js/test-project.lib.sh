350093dbbe8f2f0a80ba795ef1882319870ef853
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-01-31 19:23:16 +0900

    .

--- js/test-project.lib.sh
+++ js/test-project.lib.sh
@@ -2,6 +2,19 @@
 test "${guard_9cb8c41+set}" = set && return 0; guard_9cb8c41=x
 set -o nounset -o errexit
 
+. ./task-node.lib.sh
+
 test_my_ip_addr() {
-  sh task.sh my_ip_addr
+  "$SH" task.sh my_ip_addr
+}
+
+subcmd_js_yaml() {
+  run_node_modules_bin js-yaml bin/js-yaml.js "$@"
+}
+
+test_npx_in_subdir() {
+  cd ./subdir/
+  subcmd_node --help
+  subcmd_js_yaml --help
+  cd ..
 }
--- lib/task-node.lib.sh
+++ lib/task-node.lib.sh
@@ -22,15 +22,15 @@ subcmd_node() { # Run Node.js.
 
 task_npm__depinstall() { # Install the npm packages if the package.json is modified.
   first_call ac87fe4 || return 0
-  ! test -f package.json && return 1
-  local last_check_path=node_modules/.npm_last_check
+  ! test -f "$SCRIPT_DIR"/package.json && return 1
+  local last_check_path="$SCRIPT_DIR"/node_modules/.npm_last_check
   while true
   do
-    ! test -d node_modules/ && break
-    ! test -f package-lock.json && break
+    ! test -d "$SCRIPT_DIR"/node_modules/ && break
+    ! test -f "$SCRIPT_DIR"/package-lock.json && break
     ! test -f "$last_check_path" && break
-    newer package.json --than "$last_check_path" && break
-    newer package-lock.json --than "$last_check_path" && break
+    newer "$SCRIPT_DIR"/package.json --than "$last_check_path" && break
+    newer "$SCRIPT_DIR"/package-lock.json --than "$last_check_path" && break
     return 0
   done
   echo "Installing npm packages." >&2
@@ -44,7 +44,7 @@ run_node_modules_bin() { # Run the bin file in the node_modules.
   local bin_path="$1"
   shift
   task_npm__depinstall
-  local p=node_modules/"$pkg"/"$bin_path"
+  local p="$SCRIPT_DIR"/node_modules/"$pkg"/"$bin_path"
   if test -f "$p" && head -1 "$p" | grep -q '^#!.*node'
   then
     subcmd_node "$p" "$@"
