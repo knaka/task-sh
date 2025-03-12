e4e7390b8f501d518f521aa8d59a131c155f3da2
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-12 18:41:53 +0900

    refactor obfuscation logic to improve clarity and update test cases

--- go/cmd/obfuscate/main.go
+++ go/cmd/obfuscate/main.go
@@ -11,9 +11,6 @@ import (
 // hashingConst8 = 2**8 * (-1 + sqrt(5)) / 2
 const hashingConst8 = 158
 
-// rounds is the number of rounds to obfuscate
-const rounds = 2
-
 // Obfuscate obfuscates a byte array
 func Obfuscate(array []byte) []byte {
 	for i := range array {
@@ -35,6 +32,9 @@ func Deobfuscate(array []byte) []byte {
 	return array
 }
 
+// rounds is the number of rounds to obfuscate
+const rounds = 2
+
 // hashingConst8 = 2**32 * (-1 + sqrt(5)) / 2
 const hashhingConst32 uint32 = 2654435761
 
@@ -42,7 +42,7 @@ const hashhingConst32 uint32 = 2654435761
 func Obfuscate32(n uint32) uint32 {
 	left, right := n>>16&0xFFFF, n&0xFFFF
 	for i := range rounds {
-		left, right = right, left^(right*hashhingConst32+uint32(i))&0xFFFF
+		left, right = right, left^((right+uint32(i)+hashhingConst32)*hashhingConst32)&0xFFFF
 	}
 	return left<<16 | right
 }
@@ -51,16 +51,16 @@ func Obfuscate32(n uint32) uint32 {
 func Deobfuscate32(n uint32) uint32 {
 	left, right := n>>16&0xFFFF, n&0xFFFF
 	for i := rounds - 1; i >= 0; i-- {
-		left, right = right^(left*hashhingConst32+uint32(i))&0xFFFF, left
+		left, right = right^((left+uint32(i)+hashhingConst32)*hashhingConst32)&0xFFFF, left
 	}
 	return left<<16 | right
 }
 
 func main() {
-	for i := range 10 {
+	for _, v := range []uint32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xFFFFFFFD, 0xFFFFFFFE, 0xFFFFFFFF} {
 		fmt.Fprintf(os.Stderr, "0x%08X -> Obfuscated uint32: 0x%08X\n",
-			i,
-			Obfuscate32(uint32(i)),
+			v,
+			Obfuscate32(uint32(v)),
 		)
 	}
 }
