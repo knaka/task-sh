81aa75b9a981834d254503e74740529127d553ad
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-07-04 10:42:29 +0900

    refactor(yq): simplify yq installation
    - Remove OS detection logic.
    - Use go_os and go_arch variables.
    - Simplify download URL construction.
    - Use invoke function for command execution.

--- task-yq.lib.sh
+++ task-yq.lib.sh
@@ -2,8 +2,6 @@
 # shellcheck shell=sh
 "${sourced_0fe9d5f-false}" && return 0; sourced_0fe9d5f=true
 
-# mikefarah/yq: yq is a portable command-line YAML, JSON, XML, CSV, TOML and properties processor https://github.com/mikefarah/yq
-
 . ./task.sh
 
 # Releases · mikefarah/yq https://github.com/mikefarah/yq/releases
@@ -19,26 +17,10 @@ yq() {
   local cmd_path="$app_dir_path"/yq@"$yq_version_c887ee2""$exe_ext"
   if ! command -v "$cmd_path" >/dev/null 2>&1
   then
-    local os_arch="$(uname -s -m)"
-    local os
-    case "${os_arch% *}" in
-      (Linux) os="linux";;
-      (Darwin) os="darwin";;
-      (Windows) os="windows";;
-      (*) return 1;;
-    esac
-    local arch
-    case "${os_arch#* }" in
-      (x86_64) arch="amd64";;
-      (aarch64) arch="arm64";;
-      (*) return 1;;
-    esac
-    local url="https://github.com/mikefarah/yq/releases/download/${yq_version_c887ee2}/yq_${os}_${arch}${exe_ext}"
-    echo "Downloading yq from $url" >&2
-    curl --fail --location "$url" --output "$cmd_path"
+    curl --fail --location --output "$cmd_path" "https://github.com/mikefarah/yq/releases/download/${yq_version_c887ee2}/yq_$(go_os)_$(go_arch)$exe_ext"
     chmod +x "$cmd_path"
   fi
-  "$cmd_path" "$@"
+  invoke "$cmd_path" "$@"
 }
 
 subcmd_yq() { # Run yq(1).
