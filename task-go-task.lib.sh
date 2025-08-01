e5f8508f58a25dedc4f6cd3ee108eeab89ade1a1
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-08-01 14:32:20 +0900

    refactor: comment out delegate_tasks function in task-go-task.lib.sh and task-node-task.lib.sh

--- task-go-task.lib.sh
+++ task-go-task.lib.sh
@@ -6,24 +6,24 @@ test "${guard_c16da21+set}" = set && return 0; guard_c16da21=x
 . ./task.sh
 . ./task-go.lib.sh
 
-delegate_tasks() (
-  cd "$TASKS_DIR"
-  cmd_file=build/60f20fa"$(exe_ext)"
-  task_go_files=task.go
-  for task_go_file in task-*.go
-  do
-    if ! test -r "$task_go_file"
-    then
-      continue
-    fi
-    task_go_files="$task_go_files $task_go_file"
-  done
-  # shellcheck disable=SC2086
-  if ! test -x "$cmd_file" || newer $task_go_files --than "$cmd_file"
-  then
-    # echo Building >&2
-    # shellcheck disable=SC2086
-    subcmd_go build -o "$cmd_file" $task_go_files
-  fi
-  $cmd_file "$@"
-)
+# delegate_tasks() (
+#   cd "$TASKS_DIR"
+#   cmd_file=build/60f20fa"$(exe_ext)"
+#   task_go_files=task.go
+#   for task_go_file in task-*.go
+#   do
+#     if ! test -r "$task_go_file"
+#     then
+#       continue
+#     fi
+#     task_go_files="$task_go_files $task_go_file"
+#   done
+#   # shellcheck disable=SC2086
+#   if ! test -x "$cmd_file" || newer $task_go_files --than "$cmd_file"
+#   then
+#     # echo Building >&2
+#     # shellcheck disable=SC2086
+#     subcmd_go build -o "$cmd_file" $task_go_files
+#   fi
+#   $cmd_file "$@"
+# )
--- task-node-task.lib.sh
+++ task-node-task.lib.sh
@@ -6,6 +6,6 @@ test "${guard_2040902+set}" = set && return 0; guard_2040902=x
 . ./task.sh
 . ./task-volta.lib.sh
 
-delegate_tasks() (
-  subcmd_volta run node ./task.cjs "$@"
-)
+# delegate_tasks() (
+#   subcmd_volta run node ./task.cjs "$@"
+# )
