4dbcb8404cd0d4601fea926a4386ec4b73f20376
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-09-07 00:17:03 +0900

    feat(json2sh): add array support and delimiter configuration
    Add support for JSON arrays with indexed keys (0, 1, 2, etc.) and configurable delimiter option. Simplify jq script by removing redundant logic and using key type detection. Update documentation with array examples.

--- json2sh.lib.sh
+++ json2sh.lib.sh
@@ -4,17 +4,22 @@
 
 . ./jq.lib.sh
 
-# Convert JSON object to shell variable assignment expressions.
+# Convert JSON object or array to shell variable assignment expressions.
 # Usage: json2sh [--prefix=PREFIX] [--local]
 # Options:
 #   --prefix=PREFIX  Set variable name prefix (default: "json__")
 #   --local          Add "local" declaration to variables
-# Example:
+# Examples:
 #   echo '{"foo":{"bar":"baz"}}' | json2sh --prefix="config__" --local
 #   # Outputs: local config__foo__bar="baz"
+#   echo '["a","b","c"]' | json2sh --prefix="list__"
+#   # Outputs: list__0="a"
+#   #          list__1="b" 
+#   #          list__2="c"
 json2sh() {
   local prefix="json__"
   local local_decl=""
+  local delim="__"
   OPTIND=1; while getopts _-: OPT
   do
     if test "$OPT" = "-"
@@ -26,7 +31,8 @@ json2sh() {
     fi
     case "$OPT" in
       (local) local_decl="local ";;
-      (prefix) prefix=$OPTARG;;
+      (prefix) prefix="$OPTARG";;
+      (delim|delimiter|separator) delim="$OPTARG";;
       (\?) exit 1;;
       (*) echo "Unexpected option: $OPT" >&2; exit 1;;
     esac
@@ -34,12 +40,20 @@ json2sh() {
   shift $((OPTIND-1))
 
   # shellcheck disable=SC2016
-  jq -r --arg prefix "$prefix" --arg local_decl "$local_decl" '
+  jq -r \
+    --arg prefix "$prefix" \
+    --arg local_decl "$local_decl" \
+    --arg delim "$delim" \
+  '
     def to_sh(prefix):
-      to_entries[] |
-        (.key | gsub("[-\\.]"; "_")) as $shell_key |
-        if (.value | type == "object") then
-          .value | to_sh("\(prefix)\($shell_key)__")
+      to_entries[]
+      | if .key | type == "number" then 
+          .key | tostring
+        else 
+          .key | gsub("[-\\.]"; "_")
+        end as $shell_key
+      | if .value | type == "object" or type == "array" then
+          .value | to_sh("\(prefix)\($shell_key)\($delim)")
         else
           "\($local_decl)\(prefix)\($shell_key)=\"\(.value)\""
         end
