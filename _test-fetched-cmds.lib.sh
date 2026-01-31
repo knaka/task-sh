c0ed29a5e4d7b3815545ae1c8985f1a2e44a5064
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-01-31 19:46:20 +0900

    test: reorder

--- _test-fetched-cmds.lib.sh
+++ _test-fetched-cmds.lib.sh
@@ -5,25 +5,64 @@
 . ./task.sh
 
 . ./caddy.lib.sh
+
+test_fetched_caddy() {
+  skip_unless_full
+  caddy --help
+}  
+
 . ./chezmoi.lib.sh
-. ./node.lib.sh # volta
-. ./python.lib.sh # uv
+
+test_fetched_chezmoi() {
+  skip_unless_full
+  chezmoi --help
+}  
+
 . ./sqlc.lib.sh
-. ./sqldef.lib.sh # sqlite3def
+
+test_fetched_sqlc() {
+  skip_unless_full
+  sqlc --help
+}  
+
+. ./sqldef.lib.sh
+
+test_fetched_sqlite3def() {
+  skip_unless_full
+  sqlite3def --help
+}  
+
 . ./terraform.lib.sh
+
+test_fetched_terraform() {
+  skip_unless_full
+  terraform --help
+}  
+
+. ./python.lib.sh
+
+test_fetched_uv() {
+  skip_unless_full
+  uv --help
+}  
+
+. ./node.lib.sh
+
+test_fetched_volta() {
+  skip_unless_full
+  volta --help
+}  
+
 . ./yj.lib.sh
+
+test_fetched_yj() {
+  skip_unless_full
+  yj -h
+}  
+
 . ./yq.lib.sh
 
-test_fetched_cmds() {
+test_fetched_yq() {
   skip_unless_full
-
-  caddy --help
-  chezmoi --help
-  sqlc --help
-  sqlite3def --help
-  terraform --help
-  uv --help
-  volta --help
-  yj -h
   yq --help
-}
+}  
