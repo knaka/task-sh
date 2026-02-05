4704a154bdaee185d83269dc7009a0016be6aff3
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-05 12:54:47 +0900

    chore

--- mise-tasks/prod/build.py
+++ mise-tasks/prod/build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#MISE description="Build production environment with PowerShell."
+#MISE description="Build production environment with Python."
 #MISE tools={python="latest"}
 
 print("Build production environment with Python.")
