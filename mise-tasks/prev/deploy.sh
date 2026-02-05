f90d69bac6ea7d1571ef0038fe3b19e79db0bb10
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-05 12:05:19 +0900

    chore

--- mise-tasks/prev/deploy.sh
+++ mise-tasks/prev/deploy.sh
@@ -5,5 +5,9 @@
 
 #MISE description="Deploy preview environment."
 
-echo Deploy preview environment.
+set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
+. ./_foo.sh
+. ../_bar.sh
+cd "$1" || exit 1; shift 2
 
+echo Deploy preview environment.
